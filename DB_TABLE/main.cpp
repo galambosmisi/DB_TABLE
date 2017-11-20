75d7c1ea7f9f95b162a50d2d16c7e8d9086af758
Galambos Mihály <galambos.misi@gmail.com>
2017-11-20 12:30:52 +0100

    Initial Commit

---
+++ DB_TABLE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DB_TABLE
+//
+//  Created by Galambos Mihály on 2017. 11. 20..
+//  Copyright © 2017. Galambos Mihály. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
